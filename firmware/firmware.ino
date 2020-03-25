9d816c434f190c60c2d2706ee011303a4fc2177d
Alexandre Chartrand <alexchartrand91@gmail.com>
2020-03-24 22:45:04 -0400

    O2 controller

---
+++ firmware/firmware.ino
@@ -0,0 +1,13 @@
+
+ char print_buffer[256];
+  
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  O2Controller_Setup();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
